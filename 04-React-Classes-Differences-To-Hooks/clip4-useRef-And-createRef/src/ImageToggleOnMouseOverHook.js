2dd32bd1935dd9e8e4d0e76da80358382b0a6e98
Maxim Zubarev <maxim.zubarev.dev@icloud.com>
2019-04-28 00:41:52 +0200

    you don't need hooks nor state to make this happen.

--- 04-React-Classes-Differences-To-Hooks/clip4-useRef-And-createRef/src/ImageToggleOnMouseOverHook.js
+++ 04-React-Classes-Differences-To-Hooks/clip4-useRef-And-createRef/src/ImageToggleOnMouseOverHook.js
@@ -1,21 +1,23 @@
 import React, { useRef } from "react";
 
 const ImageToggleOnMouseOverHook = ({ primaryImg, secondaryImg }) => {
-  const imageRef = useRef(null);
+  // const imageRef = useRef(null);
 
   return (
     <div>
       <i>ImageToggleOnMouseOver - Functional Component React Hooks</i><br/>
       <img
-        onMouseOver={() => {
-          imageRef.current.src = secondaryImg;
+        onMouseOver={(e) => {
+          e.target.src = secondaryImg;
+          // imageRef.current.src = secondaryImg;
         }}
-        onMouseOut={() => {
-          imageRef.current.src = primaryImg;
+        onMouseOut={(e) => {
+          e.target.src = primaryImg;
+          // imageRef.current.src = primaryImg;
         }}
         src={primaryImg}
         alt=""
-        ref={imageRef}
+        // ref={imageRef}
       />
     </div>
   );
